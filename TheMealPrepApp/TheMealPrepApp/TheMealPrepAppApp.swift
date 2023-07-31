00d8f2852ad76b91ab363961213ee3150e46ba16
Laura Pascual <laurapascualmateo@gmail.com>
2023-07-31 21:38:07 +0200

    Create project

---
+++ TheMealPrepApp/TheMealPrepApp/TheMealPrepAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMealPrepAppApp.swift
+//  TheMealPrepApp
+//
+//  Created by Pascual Mateo Laura on 31/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMealPrepAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheMealPrepApp/TheMealPrepAppUITests/TheMealPrepAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TheMealPrepAppUITestsLaunchTests.swift
+//  TheMealPrepAppUITests
+//
+//  Created by Pascual Mateo Laura on 31/7/23.
+//
+
+import XCTest
+
+final class TheMealPrepAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
