7f53094c75bb21fb712cf2d3baefeec590673f3d
Laura Pascual <laurapascualmateo@gmail.com>
2023-07-31 22:12:08 +0200

    Add Meal model

---
+++ TheMealPrepApp/TheMealPrepApp/Domain/Models/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  TheMealPrepApp
+//
+//  Created by Pascual Mateo Laura on 31/7/23.
+//
+
+import Foundation
+
+struct Meal {
+    let name: String
+    let instructions: String
+    let video: URL
+}
